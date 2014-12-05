26616da6392726591e7bb18403929f6be7bb57ae
mihaineacsu <mihai.neacsu@ymail.com>
2014-12-05 12:05:54 +0200

    Fix typo, sent status code on get '/' was invalid

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express'),
     router = express.Router();
 
 router.get('/', function(req, res){
-	res.status(20).send({title: 'Welcome'})});
+	res.status(200).send({title: 'Welcome'})});
 //res.render('indexTemplate', {});
 
 module.exports = router;
