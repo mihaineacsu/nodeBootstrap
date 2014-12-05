b45c256566dcfff9ef04da5a431b6e62c54da328
mihaineacsu <mihai.neacsu@ymail.com>
2014-12-05 13:55:55 +0200

    Testing out heroku

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/', routes);
 
-mongoose.connect('mongodb://localhost/test')
+// mongoose.connect('mongodb://localhost/test')
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-	  console.log('mongoose: all good');
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function callback () {
+// 	  console.log('mongoose: all good');
 });
 
-app.get('/users', function(req, res) {
-	mongoose.model('users').find(function(err, users){
-		res.send(users);
-	})});
+// app.get('/users', function(req, res) {
+// 	mongoose.model('users').find(function(err, users){
+// 		res.send(users);
+// 	})});
 
 app.listen(8000);
